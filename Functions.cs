6b8b7b3dbe42b890969237362bbe488b55c37a05
Abdo <abdomoon100@gmail.com>
2024-05-20 22:40:21 +0300

    New Project

---
+++ Functions.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+namespace Functions;
+public class Function
+{
+    // to open insta
+    public static void OpenLink(string link)
+    {
+        try
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = link,
+                UseShellExecute = true
+            });
+        }
+        catch
+        {
+            Console.WriteLine("Couldn't Open Instgram");
+        }
+    }
+    // to delete the files
+    public static void CleanUp()
+    {
+        string pcuser = Environment.UserName;
+
+        string temp1 = @$"C:\Users\{pcuser}\AppData\Local\Temp";
+        string temp2 = @"C:\Windows\Temp";
+        string prefetch = @"C:\Windows\Prefetch";
+
+        CleanDirectory(temp1);
+        CleanDirectory(temp2);
+        CleanDirectory(prefetch);
+
+         static void CleanDirectory(string directoryPath)
+        {
+
+            // Delete files
+            foreach (string file in Directory.GetFiles(directoryPath))
+            {
+                try
+                {
+                File.Delete(file);
+                } catch
+                {
+                }
+            }
+
+            // Delete subdirectories
+            foreach (string subdirectory in Directory.GetDirectories(directoryPath))
+            {
+                try
+                {
+                Directory.Delete(subdirectory, true);
+                } 
+                catch
+                { 
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Spectre.Console;
+using Functions;
+class Program
+{
+    public static void Main()
+    {
+        while (true)
+        {
+            Console.ForegroundColor  = ConsoleColor.Magenta;
+            Console.Clear();
+
+            // Create and configure the table
+            Table table = new();
+            table.AddColumn(new TableColumn("Choose: 1 - 2 - 3").Width(30).Centered());
+            table.AddRow("1. Start Clean");
+            table.AddRow("2. Instagram");
+            table.AddRow("3. Close App");
+
+            table.Border = TableBorder.Rounded;
+            // Center align the table
+            table.Centered();
+            AnsiConsole.Write(table);
+            AnsiConsole.Write(new Markup("[bold darkviolet]Made by 3Bosh_Hani[/]").Centered());
+            ConsoleKeyInfo pressed = Console.ReadKey();
+            switch (pressed.Key)
+            {
+                // the clear
+                case ConsoleKey.D1:
+                    Function.CleanUp();
+                    Console.Clear();
+                    AnsiConsole.Write(new Markup("[bold darkviolet] Cleaning Done ! [/]").Centered());
+                    AnsiConsole.Write(new Markup(" ").Centered());
+                    AnsiConsole.Write(new Markup("[bold darkviolet] Press Any Key To Close ! [/]").Centered());
+                    Console.ReadKey();  
+                    return;
+                    // insta
+                case ConsoleKey.D2:
+                    Function.OpenLink("https://www.instagram.com/3bosh_hani/");
+                    break;
+                    // close
+                case ConsoleKey.D3:
+                    return;  
+                default:    
+                    Console.WriteLine("Please Press 1 - 2 - 3");
+                    break;
+            }
+
+        }
+    }
+}
